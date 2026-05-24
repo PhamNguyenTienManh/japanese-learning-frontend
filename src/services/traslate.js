5517d11ac3b59bf0555d8b28f63cc988c90bc9f3
MinhNguyenCR <minh.dox.555@gmail.com>
2026-05-24 10:49:12 +0700

    feat: update payment methods to include ZaloPay; remove MoMo and VNPay options; adjust related components and services

--- src/services/traslate.js
+++ src/services/traslate.js
@@ -1,4 +1,6 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL_API;
+
+const ARGOS_SERVER = process.env.ARGOS_SERVER;
 export async function translateText(text, source, target) {
     const response = await fetch(`${BASE_URL}/translate`, {
         method: "POST",
